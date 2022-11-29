d1a0358e5e344ae7e66f18126d19896cb4a18f0b
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-11-29 11:23:02 +0100

    test: ninth exercise

---
+++ insert-employee.sql
@@ -0,0 +1,18 @@
+INSERT into employees
+values (
+        9,
+        'Joe',
+        'John',
+        'IT Developer',
+        6,
+        '1999-03-18 00:00:00',
+        '2020-03-18 00:00:00',
+        '142 rue Sainte-Catherine',
+        'Bordeaux',
+        'NA',
+        'France',
+        33000,
+        '+33 6 77 88 99 10',
+        '+33 5 68 89 90 90',
+        'john.doe@gmail.com'
+       );
