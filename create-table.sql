40f7481e1091e3387517215ba7aee226a7fb6069
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-11-29 11:00:49 +0100

    add: fourth exercise

---
+++ create-table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE students (
+    StudentId INT NOT NULL PRIMARY KEY,
+    FirstName VARCHAR(80) NOT NULL,
+    LastName VARCHAR(80) NOT NULL,
+    Birthday DATE NOT NULL  ,
+    City VARCHAR(50) NOT NULL
+)
