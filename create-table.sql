a6fd991834e2958102fb72e155620e9ef1e4fc4b
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-11-29 11:01:57 +0100

    test: fourth exercise

--- create-table.sql
+++ create-table.sql
@@ -1,7 +1,8 @@
-CREATE TABLE students (
-    StudentId INT NOT NULL PRIMARY KEY,
+CREATE TABLE students
+(
+    StudentId INTEGER     NOT NULL PRIMARY KEY,
     FirstName VARCHAR(80) NOT NULL,
-    LastName VARCHAR(80) NOT NULL,
-    Birthday DATE NOT NULL  ,
-    City VARCHAR(50) NOT NULL
+    LastName  VARCHAR(80) NOT NULL,
+    Birthday  DATE        NOT NULL,
+    City      VARCHAR(50) NOT NULL
 )
