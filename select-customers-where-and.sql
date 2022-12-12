e6a68e90ae080b5ee83fca998c86df79dbd4822b
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-12-12 09:35:17 +0100

    test: 15 exercise

--- select-customers-where-and.sql
+++ select-customers-where-and.sql
@@ -1,4 +1,6 @@
-SELECT c.FirstName, c.LastName
+SELECT *
 FROM customers c
-join employees e on e.EmployeeId = c.SupportRepId
-where e.FirstName LIKE 'Jane' AND e.LastName LIKE 'Peacock'
+         join employees e on e.EmployeeId = c.SupportRepId
+where e.FirstName LIKE 'Jane'
+  AND e.LastName LIKE 'Peacock'
+  AND c.Country = 'USA'
