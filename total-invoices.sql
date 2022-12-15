61fd26c13c8b2623f52162f2daa392a65608adc9
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-12-15 16:16:14 +0100

    test: last third quest

--- total-invoices.sql
+++ total-invoices.sql
@@ -1,5 +1,5 @@
 SELECT FirstName || ' ' || upper(LastName)
-    AS 'FullName', round(SUM(Total),2) as 'AllInvoices'
+    AS 'FullName', SUM(Total) as 'AllInvoices'
 FROM customers
          JOIN invoices i on customers.CustomerId = i.CustomerId
 GROUP BY FullName
