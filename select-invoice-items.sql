1bc8c921645bd1996b21f5a307cf99e661e201a2
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-12-15 15:10:12 +0100

    test: sixth of third quest

--- select-invoice-items.sql
+++ select-invoice-items.sql
@@ -1,4 +1,4 @@
-SELECT i.InvoiceId, t.Name, t.UnitPrice
+SELECT i.InvoiceId as 'InvoiceId', t.Name as 'InvoiceItem', t.UnitPrice
 from invoices i
          join invoice_items ii on i.InvoiceId = ii.InvoiceId
          join tracks t on t.TrackId = ii.TrackId
