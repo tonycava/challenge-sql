3fb10170203d85b91cf2947e771acdce00ae760b
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-12-15 15:32:26 +0100

    test: seventh of third quest

--- format-date.sql
+++ format-date.sql
@@ -1,3 +1,3 @@
-SELECT EmployeeId, LastName, FirstName, STRFTIME('%d/%m/%Y', HireDate)
+SELECT EmployeeId, LastName, FirstName, STRFTIME('%d/%m/%Y', HireDate) as 'HireFrenchDate'
 from employees
 order by HireDate
