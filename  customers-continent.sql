97285e66479e81e573f39867aefb115169c211db
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2023-01-09 10:29:02 +0100

    test: third of last quest

---
+++  customers-continent.sql
@@ -0,0 +1,33 @@
+SELECT FirstName,
+       LastName,
+       Country,
+       CASE
+           WHEN Country == 'Brazil' or
+                Country == 'Canada' or
+                Country == 'USA' or
+                Country == 'Australia' or
+                Country == 'Argentina' or
+                Country == 'Chile'
+                THEN 'America'
+           WHEN Country == 'Germany' or
+                Country == 'Norway' or
+                Country == 'Austria' or
+                Country == 'Austria' or
+                Country == 'Belgium' or
+                Country == 'Denmark' or
+                Country == 'Portugal' or
+                Country == 'Czech Republic' or
+                Country == 'France' or
+                Country == 'Finland' or
+                Country == 'Hungary' or
+                Country == 'Ireland' or
+                Country == 'Italy' or
+                Country == 'Netherlands' or
+                Country == 'Poland' or
+                Country == 'Spain' or
+                Country == 'Sweden' or
+                Country == 'United Kingdom' or
+                Country == 'India'
+               THEN 'Europe'
+           END as 'Continent'
+from customers
