a9792ba6b629e40d540ed5385d2616e351ac7bf6
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2023-01-09 11:34:08 +0100

    test: fifth of last quest

---
+++ artist-albums-case.sql
@@ -0,0 +1,11 @@
+SELECT Name,
+       count(a.ArtistId) as 'NbAlbums',
+       CASE
+           WHEN count(a.ArtistId) == 1 THEN 'Unproductive'
+           WHEN count(a.ArtistId) > 1 AND count(a.ArtistId) < 10 THEN 'Productive'
+           WHEN count(a.ArtistId) >= 10 THEN 'Very Productive'
+    END as 'IsProductive '
+from artists
+         join albums a on artists.ArtistId = a.ArtistId
+group by a.ArtistId
+LIMIT 100
