0a99466abab8763ec2d6c95f70731dfddef20cbd
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2023-01-09 09:56:58 +0100

    test: first of last quest

--- invoices-price-case.sql
+++ invoices-price-case.sql
@@ -3,5 +3,5 @@ CASE
         WHEN Total < 5 THEN 'Price lower than 5$'
         WHEN Total < 10 THEN 'Price lower than 10$'
         ELSE 'Price greater than 10$'
-END AS 'Case'
+END AS 'CASE'
 FROM invoices
