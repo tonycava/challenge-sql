087df200d7d8969ee6a0b1a2b97c2a567b5a9bc5
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2023-01-09 10:15:36 +0100

    test: first of last quest

---
+++  update-invoices-case.sql
@@ -0,0 +1,7 @@
+UPDATE invoices
+SET BillingCountry =
+        CASE
+            WHEN BillingCountry == 'France' THEN Total + 3
+            WHEN BillingCountry == 'USA' THEN Total + 5
+            ELSE Total + 1
+        END
